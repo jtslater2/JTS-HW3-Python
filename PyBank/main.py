06a19cf9f23ba3c6874c9f5480f158a2d2ad44e3
unknown <jtslater2@gmail.com>
2020-09-29 10:57:38 -0400

    PyBank finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,11 @@
 import os
 import csv
 
+#Variables and lists
 monthcount = 0
 totalprofit = 0
-greatincrease = 0
-greatdecrease = 0
-increase = 0
-decrease = 0
-profit = 0
 profitlist = []
-avgtotal = 0
-avglist = []
 m2mavg = []
-m2mname = []
-#2mbest = ["year-month", "0"]
-#m2mleast = ["year-month", "0"]
 m2mtotal = 0.0
 m2mchange = 0.0
 monthlist = []
@@ -23,104 +14,50 @@ m2mbestm = ""
 m2mleastv = 0.0
 m2mleastm = ""
 
-
-
-
-
-
-
+#Find the relative path, open file and set the header
 budget_csv = os.path.join("Resources", "budget_data.csv")
-# line should be budget_csv = os.path.join("..", "Resources", "budget_data.csv")
 
 with open(budget_csv, "r") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     csv_header = next(csv_file)
 
-    #with open(cereal_csv) as csv_file:
-    #   csv_reader = csv.reader(csv_file, delimiter=",")
-    #   csv_header = next(csv_file)
+    #Start For loop to gather monthcount, total profit, profitlist and monthlist
     for row in csv_reader:
+        #variable to count months
         monthcount += 1
-        #monthcount += 1   why doesn't this work
-        #print(monthcount)
+        
+        #varible to sum total profit
         totalprofit = totalprofit + float(row[1])
-        #print(totalprofit)
+
+        #create profitlist and monthlist
         profitlist.append(row[1])
         monthlist.append(row[0])
-        #print(profitlist)
         
-        
-        #nextrow = next(csv_reader)
-        #print(nextrow)
-
-
-
-        #   temp   profit = (int(row[1]) + int(nextrow[1]))/2
-        #   temp   print(profit)
+    #Start count loop to calculate m2mchange, m2mtotal, m2m average & get best/least months
     for i in range((monthcount-1)):
         
-        #print(i)
-        #check = input("this is i")
-        #print(len(profitlist))
-        #check = input("this is profitlist length")
-
-        #print (profitlist[i])
-        #print (profitlist[i+1])
-        
-        #  don't need print((float(profitlist[i])+float(profitlist[i+1]))/2)
-
-        #check = input("this is float i and  i+1 ")
-
+        #m2m change calculated
         m2mchange = float(profitlist[i+1])-float(profitlist[i])
-        #print (m2mchange)
-        #check = input('this is m2mchange')
-
+        
+        #set m2m value & date for best month
         if m2mchange > m2mbestv:
-            # if m2mchange > float(m2mbest[1]):
             #m2mbest = [monthlist[i+1], m2mchange]
             m2mbestm = str(monthlist[i+1])
             m2mbestv = m2mchange
 
-        #print (m2mbest)
-        #check = input("this is the best m2m")
-
-        #if m2mchange < float(m2mleast[1]):
+        #set m2m value & date for worse month
         if m2mchange < m2mleastv:
             #m2mleast = [monthlist[i], m2mchange]
             m2mleastm = str(monthlist[i+1])
             m2mleastv = m2mchange
 
-        #print (m2mleast)
-        #check = input("this is the least m2m")
-
+        #set m2m total
         m2mtotal = m2mtotal + m2mchange
-        #print(m2mtotal)
-        #check = input("this is m2mtotal")
-
-
-        m2mavg = m2mtotal/(monthcount-1)#  
-        #print (m2mavg)
-        #check = input("this is the m2m change average")
-        #avgtotal = float(profitlist(i) + profitlist[i+1]
-        #print(avgtotal)
-        #check2 = input("this is avg total")
-        #avechange = (profitlist[i] + profitlist[i+1])/2
-        #print(avechange)
-        #check = input("is this right")
-        #nextentry = int(entry)+1
-
         
-        #print(nextentry)
-        #nextentry = str(nextentry)
-
-
-        #check = input("is this right")
-        #avgchange = int(profitlist[int(entry)])
-        #print(avgchange)
-    #m2mavg = m2mtotal/len(monthlist-2)
-    #print (m2mavg)
-    #check = input("this is the m2m change average")
-
+        #set m2m average
+        m2mavg = m2mtotal/(monthcount-1)#  
+        
+    #Print results to terminal    
     print("Financial Analysis")
     print("-------------------------------------------------------")
     print(f" Total Months:   {monthcount}")
@@ -128,9 +65,10 @@ with open(budget_csv, "r") as csv_file:
     print(f" Greatest Increase in Profits:   {m2mbestm}   ${int(m2mbestv)}")
     print(f" Greatest Decrease in Profits:   {m2mleastm}   ${int(m2mleastv)}")
     
-    
+    #create and open .txt file
     file = open("PyBankoutfile.txt","w+") 
  
+    #print results to .txt file
     file.write("Financial Analysis\n")
     file.write("-------------------------------------------------------\n") 
     file.write(f" Total Months:   {monthcount}\n")
